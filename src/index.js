77eacbeacae8f7cf431a138d4a1031522f04e13e
Deepak Kumar <d1612k@gmail.com>
2019-06-02 15:54:32 +0530

    added redux devtools and middleware

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import reducer from "store/reducer";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const store = createStore(reducer);
+const logger = store => next => action => {
+	console.log(action);
+	const result = next(action);
+	console.log(store.getState());
+	return result;
+};
+
+// const store = createStore(reducer, applyMiddleware(logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 
 const app = (
 	<Provider store={store}>
